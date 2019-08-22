57ff14727633b9ec856f96d5877168e9889a495e
amirsdream <amir@betik.com.tr>
2019-08-22 11:05:14 +0300

    dotnet kafka

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using LiveMusicFinder.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveMusicFinder.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<LiveShow> LiveShows { get; set; }
+    }
+}
